8feccb0a46b52f8f27b591b9fd9ad49f434c97fd
sumangal44 <sumangalkaran44@gmail.com>
2024-10-21 17:54:17 +0530

    first commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import PWABadge from './PWABadge.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
